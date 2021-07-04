811187aa4b47be651213ee03e5c02fbc8bf5bc30
La Elizabeth <aoi.yamiko07@gmail.com>
2021-07-04 11:46:04 -0400

    Adding components to create a carousel

---
+++ src/components/Categories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../assets/styles/components/Categories.scss'
+
+const Categories = ({ children }) => (
+    <div className="categories">
+        <h3 class="categories__title">Title</h3>
+        {children}
+    </div>
+
+);
+
+
+export default Categories;
+
